3d21ed2c4073e190b2ce49b3d05db062ab22dc07
gaofeifan <gaofeifan@papayixing.com>
2018-05-10 17:15:44 +0800

    添加mapper

--- common-service/common-server/src/main/java/cn/linkmore/common/dao/master/BaseDictGroupMasterMapper.java
+++ common-service/common-server/src/main/java/cn/linkmore/common/dao/master/BaseDictGroupMasterMapper.java
@@ -1,20 +1,11 @@
 package cn.linkmore.common.dao.master;
 
-import java.util.List;
-
-import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
+
 import cn.linkmore.common.entity.BaseDictGroup;
-import cn.linkmore.common.response.ResCauseBean;
-import cn.linkmore.common.response.ResCustomerInfoBean;
 
 @Mapper
 public interface BaseDictGroupMasterMapper {
     BaseDictGroup selectByPrimaryKey(Long id);
 
-    List<ResCauseBean> findByCause(Map<String,Object> param);
-    
-    List<ResCustomerInfoBean> findCustomerBean(List<String> list);
-
-	List<ResCauseBean> findByCode(Map<String, Object> param);
 }
