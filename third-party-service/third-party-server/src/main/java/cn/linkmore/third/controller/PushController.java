16d10a95a6cb8768c4fd135b0d26367b02e5f705
liwenlong <liwenlong@papayixing.com>
2018-05-15 16:57:48 +0800

    修正代码[推送]

--- third-party-service/third-party-server/src/main/java/cn/linkmore/third/controller/PushController.java
+++ third-party-service/third-party-server/src/main/java/cn/linkmore/third/controller/PushController.java
@@ -1,7 +1,15 @@
 package cn.linkmore.third.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import cn.linkmore.third.request.ReqPush;
+import cn.linkmore.third.service.PushService;
 /**
  * Controller -推送服务
  * @author liwenlong
@@ -10,6 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/third/push")
 public class PushController {
-	public void push() {
+	
+	@Autowired
+	private PushService pushService;
+	
+	
+	@RequestMapping(value = "/v2.0", method = RequestMethod.POST) 
+	public void push(@RequestBody ReqPush rp) {
+		this.pushService.push(rp);
+	}
+
+	@RequestMapping(value = "/v2.0", method = RequestMethod.PUT) 
+	public void push(@RequestBody List<ReqPush> rps) {
+		this.pushService.push(rps);
 	}
 }
