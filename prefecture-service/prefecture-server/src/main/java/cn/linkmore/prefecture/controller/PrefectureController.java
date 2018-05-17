d0c6216765b9eef71674d6ff2fa0d77bdf3f14ec
jiaohanbin <jiaohanbin@papayixing.com>
2018-05-17 18:50:59 +0800

    删除多余引用

--- prefecture-service/prefecture-server/src/main/java/cn/linkmore/prefecture/controller/PrefectureController.java
+++ prefecture-service/prefecture-server/src/main/java/cn/linkmore/prefecture/controller/PrefectureController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import cn.linkmore.account.client.hystrix.UserClientHystrix;
 import cn.linkmore.account.response.ResUser;
 import cn.linkmore.prefecture.request.ReqCity;
