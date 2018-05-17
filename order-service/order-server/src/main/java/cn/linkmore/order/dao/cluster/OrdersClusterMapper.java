c43a1c122a1e8f60ed09a5495ef75de38cadac5b
jiaohanbin <jiaohanbin@papayixing.com>
2018-05-17 19:01:11 +0800

    增加mapper映射

--- order-service/order-server/src/main/java/cn/linkmore/order/dao/cluster/OrdersClusterMapper.java
+++ order-service/order-server/src/main/java/cn/linkmore/order/dao/cluster/OrdersClusterMapper.java
@@ -1,7 +1,9 @@
 package cn.linkmore.order.dao.cluster;
 
-import cn.linkmore.order.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
 
+import cn.linkmore.order.entity.Orders;
+@Mapper
 public interface OrdersClusterMapper {
 	/**
 	 * 根据主键id查询订单信息
--- order-service/order-server/src/main/java/cn/linkmore/order/dao/cluster/OrdersDetailClusterMapper.java
+++ order-service/order-server/src/main/java/cn/linkmore/order/dao/cluster/OrdersDetailClusterMapper.java
@@ -1,7 +1,9 @@
 package cn.linkmore.order.dao.cluster;
 
-import cn.linkmore.order.entity.OrdersDetail;
+import org.apache.ibatis.annotations.Mapper;
 
+import cn.linkmore.order.entity.OrdersDetail;
+@Mapper
 public interface OrdersDetailClusterMapper {
 
     OrdersDetail findById(Long id);
