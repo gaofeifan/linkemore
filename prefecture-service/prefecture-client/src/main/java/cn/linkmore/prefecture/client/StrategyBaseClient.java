2798010203e0e10e17ea76bc59513cb23deae9c7
jiaohanbin <jiaohanbin@papayixing.com>
2018-05-19 13:13:18 +0800

    修改prefecture client方法

--- prefecture-service/prefecture-client/src/main/java/cn/linkmore/prefecture/client/StrategyBaseClient.java
+++ prefecture-service/prefecture-client/src/main/java/cn/linkmore/prefecture/client/StrategyBaseClient.java
@@ -1,15 +1,13 @@
 package cn.linkmore.prefecture.client;
 
-import java.util.Date;
 import java.util.Map;
-
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import cn.linkmore.feign.FeignConfiguration;
 import cn.linkmore.prefecture.client.hystrix.StrategyBaseClientHystrix;
+import cn.linkmore.prefecture.request.ReqStrategy;
 /**
  * 远程调用 - 计费详情
  * @author jiaohanbin
@@ -26,9 +24,7 @@ public interface StrategyBaseClient {
 	 * @param beginTime Date
 	 * @param endTime Date
 	 */
-	@RequestMapping(value = "/v2.0/fee", method=RequestMethod.GET)
-	public Map<String, Object> fee(@RequestParam("strategyId") Long strategyId, 
-			@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") Date beginTime,
-			@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") Date endTime);
+	@RequestMapping(value = "/v2.0/fee", method=RequestMethod.POST)
+	public Map<String, Object> fee(@RequestBody ReqStrategy reqStrategy);
 	
 }
