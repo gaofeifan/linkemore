92c03e987aff384a1f794ef07632e3314add595f
liwenlong <liwenlong@papayixing.com>
2018-05-23 15:13:18 +0800

    添加getter，setter

--- user-app-server/src/main/java/cn/linkmore/user/response/ResPrefecture.java
+++ user-app-server/src/main/java/cn/linkmore/user/response/ResPrefecture.java
@@ -3,7 +3,7 @@ package cn.linkmore.user.response;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel("车区地图")
+@ApiModel("地图车区信息")
 public class ResPrefecture { 
 	@ApiModelProperty(value = "主键")
 	private Long id; 
@@ -83,6 +83,69 @@ public class ResPrefecture {
 	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}
-	
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(Long cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getFirstHour() {
+		return firstHour;
+	}
+
+	public void setFirstHour(String firstHour) {
+		this.firstHour = firstHour;
+	}
+
+	public String getTimelyLong() {
+		return timelyLong;
+	}
+
+	public void setTimelyLong(String timelyLong) {
+		this.timelyLong = timelyLong;
+	}
+
+	public String getTimelyUnit() {
+		return timelyUnit;
+	}
+
+	public void setTimelyUnit(String timelyUnit) {
+		this.timelyUnit = timelyUnit;
+	}
+
+	public Long getFreeMins() {
+		return freeMins;
+	}
+
+	public void setFreeMins(Long freeMins) {
+		this.freeMins = freeMins;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public short getLimitStatus() {
+		return limitStatus;
+	}
+
+	public void setLimitStatus(short limitStatus) {
+		this.limitStatus = limitStatus;
+	} 
 	
 }
