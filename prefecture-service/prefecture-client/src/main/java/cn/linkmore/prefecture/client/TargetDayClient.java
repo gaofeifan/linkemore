10f278e8c530f64c1c194e3f742fc8d3c0893e56
jiaohanbin <jiaohanbin@papayixing.com>
2018-06-05 14:53:57 +0800

    提交

--- prefecture-service/prefecture-client/src/main/java/cn/linkmore/prefecture/client/TargetDayClient.java
+++ prefecture-service/prefecture-client/src/main/java/cn/linkmore/prefecture/client/TargetDayClient.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import cn.linkmore.bean.view.ViewPage;
 import cn.linkmore.bean.view.ViewPageable;
 import cn.linkmore.feign.FeignConfiguration;
-import cn.linkmore.prefecture.client.hystrix.LockOperateLogClientHystrix;
+import cn.linkmore.prefecture.client.hystrix.TargetDayClientHystrix;
 /**
  * 远程调用 - 车区每日目标
  * @author jiaohanbin
  * @version 2.0
  *
  */ 
-@FeignClient(value = "prefecture-server", path = "/target_day", fallback=LockOperateLogClientHystrix.class,configuration = FeignConfiguration.class)
+@FeignClient(value = "prefecture-server", path = "/target_day", fallback=TargetDayClientHystrix.class,configuration = FeignConfiguration.class)
 public interface TargetDayClient {
 	
 	@RequestMapping(value = "/v2.0/list", method = RequestMethod.POST)
