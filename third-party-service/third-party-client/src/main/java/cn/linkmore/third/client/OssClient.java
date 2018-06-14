3489d3dc6795ad36d63c968b256888200228f0b8
jiaohanbin <jiaohanbin@papayixing.com>
2018-06-14 18:53:05 +0800

    解决冲突

--- third-party-service/third-party-client/src/main/java/cn/linkmore/third/client/OssClient.java
+++ third-party-service/third-party-client/src/main/java/cn/linkmore/third/client/OssClient.java
@@ -1,10 +1,12 @@
 package cn.linkmore.third.client;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.aliyun.oss.OSSClient;
 import cn.linkmore.feign.FeignConfiguration;
-import cn.linkmore.third.client.hystrix.OssClientHystrix;
- 
+import cn.linkmore.third.client.hystrix.OssClientHystrix; 
 
 /**
  * Client - Oss文件服务
@@ -12,7 +14,14 @@ import cn.linkmore.third.client.hystrix.OssClientHystrix;
  * @version 2.0
  *
  */
-@FeignClient(value = "third-party-server", path = "/oss", fallback=OssClientHystrix.class,configuration = FeignConfiguration.class)
+@FeignClient(value = "third-party-server", path = "/feign/oss", fallback=OssClientHystrix.class,configuration = FeignConfiguration.class)
 public interface OssClient {
+	@RequestMapping(value = "/v2.0/upload", method = RequestMethod.GET) 
+	@ResponseBody
+	public OSSClient uploadOSSClient();
 
+	@RequestMapping(value = "/v2.0/download", method = RequestMethod.GET) 
+	@ResponseBody
+	public OSSClient downloadOSSClient();
+	
 }
--- third-party-service/third-party-client/src/main/java/cn/linkmore/third/client/WechatClient.java
+++ third-party-service/third-party-client/src/main/java/cn/linkmore/third/client/WechatClient.java
@@ -1,7 +1,10 @@
 package cn.linkmore.third.client;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import cn.linkmore.feign.FeignConfiguration;
 import cn.linkmore.third.client.hystrix.WechatClientHystrix;
 
@@ -10,7 +13,11 @@ import cn.linkmore.third.client.hystrix.WechatClientHystrix;
  * @author liwenlong
  * @version 2.0
  */
-@FeignClient(value = "third-party-server", path = "/wechat", fallback=WechatClientHystrix.class,configuration = FeignConfiguration.class)
+@FeignClient(value = "third-party-server", path = "/feign/wechat", fallback=WechatClientHystrix.class,configuration = FeignConfiguration.class)
 public interface WechatClient {
-
+	
+	@RequestMapping(method=RequestMethod.GET)
+	@ResponseBody
+	public String getTicket(@RequestParam("actionName") String actionName,@RequestParam("sceneId")Long sceneId);
+	
 }
