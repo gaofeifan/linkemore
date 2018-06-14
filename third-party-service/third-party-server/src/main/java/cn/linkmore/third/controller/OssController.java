d8ac9a56e61bccd4ce3dbf9f400675770d377e56
jiaohanbin <jiaohanbin@papayixing.com>
2018-06-14 18:53:51 +0800

    commit

--- third-party-service/third-party-server/src/main/java/cn/linkmore/third/controller/OssController.java
+++ third-party-service/third-party-server/src/main/java/cn/linkmore/third/controller/OssController.java
@@ -1,14 +1,40 @@
 package cn.linkmore.third.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.aliyun.oss.OSSClient;
+
+import cn.linkmore.third.config.OssConfig;
+import cn.linkmore.third.service.OssService;
 /**
  * Controller - OSS服务
  * @author liwenlong
  * @version 2.0
  */
 @RestController
-@RequestMapping("/oss")
+@RequestMapping("/feign/oss")
 public class OssController {
+	@Autowired
+	private OssService ossService;
+	
+	@RequestMapping(value = "/v2.0/upload", method = RequestMethod.GET) 
+	@ResponseBody
+	public OSSClient uploadOSSClient(){
+		return this.ossService.uploadOSSClient();
+	}
 
+	@RequestMapping(value = "/v2.0/download", method = RequestMethod.GET) 
+	@ResponseBody
+	public OSSClient downloadOSSClient(){
+		return this.ossService.downloadOSSClient();
+	}
+	
+	@RequestMapping(value = "/v2.0/init", method = RequestMethod.GET) 
+	@ResponseBody
+	public OssConfig initOssConfig(){
+		return this.ossService.ossConfig();
+	}
 }
--- third-party-service/third-party-server/src/main/java/cn/linkmore/third/controller/WechatController.java
+++ third-party-service/third-party-server/src/main/java/cn/linkmore/third/controller/WechatController.java
@@ -1,7 +1,12 @@
 package cn.linkmore.third.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import cn.linkmore.third.service.WechatService;
 /**
  * Controller - 微信服务号
  * @author liwenlong
@@ -9,7 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
-@RequestMapping("/wechat")
+@RequestMapping("/feign/wechat")
 public class WechatController {
+	@Autowired
+	private WechatService wechatService;
 	
+	@RequestMapping(method=RequestMethod.GET)
+	@ResponseBody
+	public String getTicket(@RequestParam("actionName") String actionName,@RequestParam("sceneId")Long sceneId) {
+		return wechatService.getTicket(actionName, sceneId);
+	}
 }
