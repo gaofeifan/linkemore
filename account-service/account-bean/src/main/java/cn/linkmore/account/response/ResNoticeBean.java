cd242b0bc39f4407bd45253fe8da2c689fd787df
gaofeifan <gaofeifan@papayixing.com>
2018-06-27 19:06:11 +0800

    添加res

---
+++ account-service/account-bean/src/main/java/cn/linkmore/account/response/ResNoticeBean.java
@@ -0,0 +1,96 @@
+package cn.linkmore.account.response;
+
+import java.util.Date;
+
+
+public class ResNoticeBean {
+	private Long id;
+	// 分类
+	private Integer type;
+	// 标题
+	private String title;
+	// 描述
+	private String description;
+	// 内容
+	private String content;
+	// url
+	private String url;
+	// 状态
+	private Integer status;// 0已发送，1未发送
+	// 创建时间
+	private Date createTime;
+	// 推送时间
+	private Date pushTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getPushTime() {
+		return pushTime;
+	}
+
+	public void setPushTime(Date pushTime) {
+		this.pushTime = pushTime;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
