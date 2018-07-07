d5d8f4d4755477dd8101e6ef8be37bd28ede4c0c
gaofeifan <gaofeifan@papayixing.com>
2018-07-07 15:10:01 +0800

    添加第三方方法

--- third-party-service/third-party-server/src/main/java/cn/linkmore/third/service/WechatService.java
+++ third-party-service/third-party-server/src/main/java/cn/linkmore/third/service/WechatService.java
@@ -1,6 +1,12 @@
 package cn.linkmore.third.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import cn.linkmore.third.request.ReqTemplateMsg;
+import cn.linkmore.third.response.ResToken;
+import cn.linkmore.third.response.ResWechatUserList;
 
 /**
  * 微信 服务处理，消息转换等
@@ -17,6 +23,27 @@ public interface WechatService {
 	 * @return
 	 */
 	public String getTicket(String actionName,Long sceneId);
+
+	/**
+	 * @Description  根据appid  appsecret  key刷新
+	 * @Author   GFF 
+	 * @Version  v2.0
+	 */
+	public ResToken resetToken(String appid, String appsecret, String key);
+
+	/**
+	 * @Description  推送微信消息
+	 * @Author   GFF 
+	 * @Version  v2.0
+	 */
+	public void pushTemplateMsg(ReqTemplateMsg msg);
+
+	/**
+	 * @Description  获取公众号关注的用户列表
+	 * @Author   GFF 
+	 * @Version  v2.0
+	 */
+	public ResWechatUserList getUserList(String token);
 }
 
 
