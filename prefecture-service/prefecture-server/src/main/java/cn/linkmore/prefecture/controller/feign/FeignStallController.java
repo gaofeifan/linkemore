3c4aff057a5169d4024fb15b13e7fd0ab794b16f
jiaohanbin <jiaohanbin@papayixing.com>
2018-07-21 14:20:36 +0800

    commit

--- prefecture-service/prefecture-server/src/main/java/cn/linkmore/prefecture/controller/feign/FeignStallController.java
+++ prefecture-service/prefecture-server/src/main/java/cn/linkmore/prefecture/controller/feign/FeignStallController.java
@@ -18,6 +18,7 @@ import cn.linkmore.bean.view.ViewPageable;
 import cn.linkmore.prefecture.request.ReqCheck;
 import cn.linkmore.prefecture.request.ReqOrderStall;
 import cn.linkmore.prefecture.request.ReqStall;
+import cn.linkmore.prefecture.response.ResStall;
 import cn.linkmore.prefecture.response.ResStallEntity;
 import cn.linkmore.prefecture.response.ResStallLock;
 import cn.linkmore.prefecture.response.ResStallOps;
@@ -129,6 +130,13 @@ public class FeignStallController {
 	public ResStallEntity findByLock(@PathVariable("sn") String sn) {
 		ResStallEntity stallEntity = this.stallService.findByLock(sn);
 		return stallEntity;
+	}
+	
+	@RequestMapping(value = "/v2.0/find_list", method = RequestMethod.POST)
+	@ResponseBody
+	public List<ResStall> findStallList(@RequestBody Map<String, Object> param){
+		List<ResStall> list = this.stallService.findList(param);
+		return list;
 	}
 	
 	@RequestMapping(value = "/v2.0/find-list", method = RequestMethod.POST)
