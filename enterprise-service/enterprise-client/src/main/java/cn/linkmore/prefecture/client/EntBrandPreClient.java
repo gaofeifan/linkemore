5280dbc0c3c11653cc31cfc7548207df7749b2b6
jiaohanbin <jiaohanbin@papayixing.com>
2018-07-24 11:37:49 +0800

    commit

---
+++ enterprise-service/enterprise-client/src/main/java/cn/linkmore/prefecture/client/EntBrandPreClient.java
@@ -0,0 +1,22 @@
+package cn.linkmore.prefecture.client;
+
+import java.util.List;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import cn.linkmore.enterprise.response.ResBrandPreStall;
+import cn.linkmore.feign.FeignConfiguration;
+import cn.linkmore.prefecture.client.hystrix.EntBrandPreClientHystrix;
+/**
+ * 远程调用 - 企业品牌车区
+ * @author jiaohanbin
+ * @version 2.0
+ *
+ */ 
+@FeignClient(value = "enterprise-server", path = "/feign/ent-brand-pre", fallback=EntBrandPreClientHystrix.class,configuration = FeignConfiguration.class)
+public interface EntBrandPreClient {
+	@RequestMapping(value = "/v2.0/pre-stall-list", method = RequestMethod.POST)
+	@ResponseBody
+	public List<ResBrandPreStall> preStallList();
+}
---
+++ enterprise-service/enterprise-server/src/main/java/cn/linkmore/enterprise/controller/feign/FeignEntBrandPreController.java
@@ -0,0 +1,32 @@
+package cn.linkmore.enterprise.controller.feign;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import cn.linkmore.enterprise.response.ResBrandPreStall;
+import cn.linkmore.enterprise.service.EntBrandPreService;
+
+/**
+ * 
+ * @Description - 品牌企业车区
+ * @Author jiaohanbin
+ * @Date 2018年1月20日 上午11:50:26
+ * @Version v1.0.0
+ */
+@Controller
+@RequestMapping("/feign/ent-brand-pre")
+public class FeignEntBrandPreController {
+	
+	@Resource
+	private EntBrandPreService entBrandPreService;
+
+	@RequestMapping(value = "/v2.0/pre-stall-list", method = RequestMethod.POST)
+	@ResponseBody
+	public List<ResBrandPreStall> preStallList() {
+		return	this.entBrandPreService.preStallList();
+	}
+
+}
