38ac266da25c0a9c1c6af5fa01563bf67ae9cada
jiaohanbin <jiaohanbin@papayixing.com>
2018-08-04 10:44:50 +0800

    add the task

--- enterprise-service/enterprise-server/src/main/java/cn/linkmore/enterprise/task/BrandExpireTask.java
+++ enterprise-service/enterprise-server/src/main/java/cn/linkmore/enterprise/task/BrandExpireTask.java
@@ -1,5 +1,6 @@
 package cn.linkmore.enterprise.task;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -13,8 +14,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import com.alibaba.fastjson.JSON;
 import cn.linkmore.enterprise.dao.cluster.EntBrandPreClusterMapper;
+import cn.linkmore.enterprise.dao.cluster.EntBrandStallClusterMapper;
 import cn.linkmore.enterprise.dao.master.EntBrandPreMasterMapper;
+import cn.linkmore.enterprise.dao.master.EntBrandStallMasterMapper;
 import cn.linkmore.enterprise.response.ResBrandPre;
+import cn.linkmore.enterprise.response.ResBrandStall;
+import cn.linkmore.prefecture.client.StallClient;
 
 @Component
 public class BrandExpireTask {
@@ -27,9 +32,18 @@ public class BrandExpireTask {
 	@Resource
 	private EntBrandPreMasterMapper entBrandPreMasterMapper;
 	
+	@Resource
+	private EntBrandStallClusterMapper entBrandStallClusterMapper;
+	
+	@Resource
+	private EntBrandStallMasterMapper entBrandStallMasterMapper;
+	
+	@Resource
+	private StallClient stallClient;
+	
 	@Scheduled(cron = "0 0/5 * * * ?")
 	public void run() {
-		log.info("sync stall lock thread...");
+		log.info("sync brand pre thread...");
 		init();
 	}
 	
@@ -45,8 +59,24 @@ public class BrandExpireTask {
 				param.put("status", 2);
 				param.put("updateTime", new Date());
 			    this.entBrandPreMasterMapper.startOrStop(param);
+			    
+			    List<ResBrandStall> stallList = this.entBrandStallClusterMapper.findByBrandPreId(pre.getId());
+				List<Long> stallIds = new ArrayList<Long>();
+				List<Long> ids = new ArrayList<Long>();
+				if(CollectionUtils.isNotEmpty(stallList)) {
+					for(ResBrandStall stall: stallList) {
+						stallIds.add(stall.getStallId());
+						ids.add(stall.getId());
+					}
+					Map<String,Object> map = new HashMap<String,Object>();
+					map.put("brand", "0");
+					map.put("list", stallIds);
+					this.stallClient.updateBrand(map);
+				}
+				if(CollectionUtils.isNotEmpty(ids)) {
+					entBrandStallMasterMapper.delete(ids);
+				}
 			}
 		}
 	}
-
 }
