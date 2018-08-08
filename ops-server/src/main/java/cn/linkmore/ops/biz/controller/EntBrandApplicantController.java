2d51bc988bd341176a8c9ddd637bed216909693a
jiaohanbin <jiaohanbin@papayixing.com>
2018-08-08 16:27:19 +0800

    commit

--- ops-server/src/main/java/cn/linkmore/ops/biz/controller/EntBrandApplicantController.java
+++ ops-server/src/main/java/cn/linkmore/ops/biz/controller/EntBrandApplicantController.java
@@ -1,38 +1,30 @@
 package cn.linkmore.ops.biz.controller;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import cn.linkmore.bean.view.ViewPage;
 import cn.linkmore.bean.view.ViewPageable;
-import cn.linkmore.ops.biz.service.ReceiveService;
+import cn.linkmore.ops.biz.service.EntBrandApplicantService;
 /**
- * 领券记录
- * @author   GFF
- * @Date     2018年5月30日
+ * 品牌申请人
+ * @author   jiaohanbin
+ * @Date     2018年6月25日
  * @Version  v2.0
  */
 @Controller
-@RequestMapping("/admin/biz_receive")
-public class ReceiveController {
-
+@RequestMapping("/admin/biz/ent-brand-applicant")
+public class EntBrandApplicantController {
+	
 	@Resource
-	private ReceiveService receiveService;
-
+	private EntBrandApplicantService entBrandApplicantService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.POST)
 	@ResponseBody
 	public ViewPage list(HttpServletRequest request, ViewPageable pageable) {
-		return receiveService.findPage(pageable);
-	}
-	
-	@RequestMapping(value = "/detailList", method = RequestMethod.POST)
-	@ResponseBody
-	public ViewPage detailList(HttpServletRequest request, ViewPageable pageable) {
-		return receiveService.findDetailPage(pageable);
+		return this.entBrandApplicantService.findPage(pageable);
 	}
 }
