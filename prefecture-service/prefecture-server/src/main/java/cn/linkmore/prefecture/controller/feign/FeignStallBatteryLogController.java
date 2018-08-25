50f9160250bc19ab36f134ffe98f12b77925f6eb
jiaohanbin <jiaohanbin@papayixing.com>
2018-08-25 14:34:26 +0800

    commit

--- prefecture-service/prefecture-server/src/main/java/cn/linkmore/prefecture/controller/feign/FeignStallBatteryLogController.java
+++ prefecture-service/prefecture-server/src/main/java/cn/linkmore/prefecture/controller/feign/FeignStallBatteryLogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import cn.linkmore.prefecture.response.ResStallBatteryLog;
 import cn.linkmore.prefecture.service.StallBatteryLogService;
 
 /**
