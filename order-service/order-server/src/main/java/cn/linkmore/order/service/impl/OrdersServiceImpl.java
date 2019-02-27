2df2cdc914603ca61d1a967a9e170c64acb83cf5
jiaohanbin <jiaohanbin@papayixing.com>
2019-02-27 18:43:10 +0800

    commit

--- order-service/order-server/src/main/java/cn/linkmore/order/service/impl/OrdersServiceImpl.java
+++ order-service/order-server/src/main/java/cn/linkmore/order/service/impl/OrdersServiceImpl.java
@@ -2322,10 +2322,10 @@ public class OrdersServiceImpl implements OrdersService {
 			if(!downStatus) {
 				if(this.redisService.exists(RedisKey.ORDER_STALL_DOWN_FAILED.key+ros.getOrderId())) {
 					Object object = this.redisService.get(RedisKey.ORDER_STALL_DOWN_FAILED.key+ros.getOrderId());
-					log.info("down flag reason = {}", JSON.toJSON(object));
+					log.info("down flag reason = {}", StatusEnum.get((int)object));
 					throw new BusinessException(StatusEnum.get((int)object));
 				}else {
-					log.info("the server is unconnecting");
+					log.info("....................the server is unconnecting");
 					this.redisService.set(RedisKey.ORDER_STALL_DOWN_FAILED.key + ros.getOrderId(), StatusEnum.DOWN_LOCK_FAIL_RETRY.code,ExpiredTime.STALL_DOWN_FAIL_EXP_TIME.time);
 					throw new BusinessException(StatusEnum.DOWN_LOCK_FAIL_RETRY);
 				}
