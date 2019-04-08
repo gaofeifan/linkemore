ead1e83437fe9ef81e616dc5293c548addd4669f
ff.gao <1315697146@qq.com>
2019-04-08 09:41:56 +0800

    commit

---
+++ enterprise-service/enterprise-server/src/main/java/cn/linkmore/enterprise/controller/app/response/ResAuthRentStall.java
@@ -0,0 +1,45 @@
+package cn.linkmore.enterprise.controller.app.response;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@ApiModel("长租车位授权")
+public class ResAuthRentStall {
+
+	@ApiModelProperty(value = "是否有未完成操作")
+	private Boolean isHave = false;
+	
+	@ApiModelProperty(value = "可用车位数量")
+	private int num;
+	
+	@ApiModelProperty(value = "用户车位数据集")
+	private List<ResRentUser> rentUsers;
+
+	public Boolean getIsHave() {
+		return isHave;
+	}
+
+	public void setIsHave(Boolean isHave) {
+		this.isHave = isHave;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public List<ResRentUser> getRentUsers() {
+		return rentUsers;
+	}
+
+	public void setRentUsers(List<ResRentUser> rentUsers) {
+		this.rentUsers = rentUsers;
+	}
+
+	
+	
+}
