5f7618fb4e86d08e2d6551d1a5129ebdd9654c54
jiaohanbin <jiaohanbin@papayixing.com>
2019-06-12 19:53:06 +0800

    长租使用记录点击导出无反应问题修复

--- ops-server/src/main/java/cn/linkmore/ops/ent/controller/RentedRecordController.java
+++ ops-server/src/main/java/cn/linkmore/ops/ent/controller/RentedRecordController.java
@@ -16,6 +16,8 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,7 +42,7 @@ import cn.linkmore.ops.utils.ExcelUtil;
 @Controller
 @RequestMapping("/admin/ent/rented-record")
 public class RentedRecordController extends BaseController{
-
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@Resource
 	private RentedRecordService rentedRecordService;
 	
@@ -78,7 +80,12 @@ public class RentedRecordController extends BaseController{
 				s.put("preName", rb.getPreName());
 				s.put("stallName", rb.getStallName());
 				s.put("downTime", sdf.format(rb.getDownTime()));
-				s.put("leaveTime", sdf.format(rb.getLeaveTime()));
+				if(rb.getLeaveTime()!=null) {
+					s.put("leaveTime", sdf.format(rb.getLeaveTime()));
+				}
+				else {
+					s.put("leaveTime", "");
+				}
 				s.put("status", str.get(rb.getStatus()));
 				ja.add(s);
 			}
@@ -113,6 +120,7 @@ public class RentedRecordController extends BaseController{
 			outputStream.flush();
 			outputStream.close();
 		} catch (Exception e) {
+			log.info("message = {} stack = {}",e.getMessage(),e.getStackTrace());
 			e.printStackTrace();
 		}
 	}
